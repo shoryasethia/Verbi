907b1aa633848854b43c475597b5e7cadb3fa8bc
Shorya Sethia <132898518+shoryasethia@users.noreply.github.com>
2026-01-24 23:47:30 +0530

    Fix transcription.py import for deepgram SDK v5

--- voice_assistant/transcription.py
+++ voice_assistant/transcription.py
@@ -8,7 +8,7 @@ import time
 from colorama import Fore, init
 from openai import OpenAI
 from groq import Groq
-from deepgram import DeepgramClient,PrerecordedOptions,FileSource
+from deepgram import DeepgramClient
 
 fast_url = "http://localhost:8000"
 checked_fastwhisperapi = False
